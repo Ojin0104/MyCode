b9c088f00949782b44a20d02aad5dd847cb235c5
yeongjin <hanyj0104@naver.com>
2023-03-16 01:16:08 +0900

    왜또

--- SamsungSDS/src/dfs/baek1189컴백홈.java
+++ SamsungSDS/src/dfs/baek1189컴백홈.java
@@ -1,2 +1,62 @@
-package dfs;public class baek1189컴백홈 {
+package dfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baek1189컴백홈 {
+    static int R;
+    static int C;
+    static int K;
+    static int answer=0;
+    static int[] n_row={1,0,-1,0};
+    static int[] n_col={0,1,0,-1};
+    static boolean check[][];
+    public static void main(String args[]) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        R=Integer.parseInt(st.nextToken());
+        C=Integer.parseInt(st.nextToken());
+        K=Integer.parseInt(st.nextToken());
+
+        char[][] arr=new char[R][C];
+        check=new boolean[R][C];
+        for(int i=0;i<R;i++){
+            st=new StringTokenizer(br.readLine());
+            String s=st.nextToken();
+            for(int j=0;j<C;j++){
+                arr[i][j]=s.charAt(j);
+            }
+        }
+        check[R-1][0]=true;
+        dfs(arr,R-1,0,1);
+        System.out.println(answer);
+    }
+    static void dfs(char[][] arr,int row,int col,int count){
+        if(count>K){
+
+            return;
+
+        }
+
+        if(row==0&&col==C-1){
+
+            if(count==K){
+                answer++;
+            }
+
+            return;
+        }
+
+        for(int i=0;i<4;i++){
+            if(row+n_row[i]<0||col+n_col[i]<0||row+n_row[i]>=R||col+n_col[i]>=C)
+                continue;
+            if(check[row+n_row[i]][col+n_col[i]]||arr[row+n_row[i]][col+n_col[i]]=='T')
+                continue;
+            check[row+n_row[i]][col+n_col[i]]=true;
+            dfs(arr,row+n_row[i],col+n_col[i],count+1);
+            check[row+n_row[i]][col+n_col[i]]=false;
+        }
+    }
 }
--- SamsungSDS/src/ehlfk.java
+++ SamsungSDS/src/ehlfk.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class ehlfk {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class ehlfk {
+    static char[][] map;
+    static StringBuilder sb= new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int size=Integer.parseInt(st.nextToken());
+        int jump=Integer.parseInt(st.nextToken());
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int[] dp=new int[size];
+        for(int i=0;i<jump;i++){
+            st=new StringTokenizer(br.readLine());
+            int first=Integer.parseInt(st.nextToken());
+            int second=Integer.parseInt(st.nextToken());
+            map.put(second,first);
+        }
+
+        for(int i=0;i<6;i++){
+            dp[i]=1;
+            if(map.containsKey(0)){//0에서점프판있으면
+                if(map.get(0)==i)
+                    dp[i]=0;
+            }
+        }
+        for(int i=6;i<size;i++){
+            dp[i]=Integer.MAX_VALUE/4;
+            if(map.containsKey(i)){
+                if(map.get(i)>i)
+                    continue;
+                dp[i]=dp[map.get(i)];
+            }
+            for(int j=1;j<=6;j++){
+                dp[i]=Math.min(dp[i-j]+1,dp[i]);
+            }
+        }
+
+    }
 }
