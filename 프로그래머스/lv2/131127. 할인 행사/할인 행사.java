24dc3284092822d912abb1b28469abd5c6635785
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-04-30 19:14:11 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -2,41 +2,51 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        HashMap<String,Integer> index=new HashMap<>();
-        
-        for(int i=0;i<want.length;i++){
-            index.put(want[i],i);
+        HashMap<String, Integer> map = new HashMap<>();
+        int cnt=0;
+        for(int i=0;i<want.length;i++) {
+            map.put(want[i],number[i]);
+            cnt+=number[i];
         }
-        int last=0;
-        int food=0;
-        for(int j=0;j<10;j++){
-            if(!index.containsKey(discount[j]))continue;
-            
-            food=index.get(discount[j]);
-            
-            number[food]-=1;
-                
+
+        for(int i=0;i<=discount.length-cnt;i++) {
+            HashMap<String, Integer> temp = new HashMap<>();
+
+            for(String key : map.keySet()) 
+                temp.put(key,map.get(key));
+
+            boolean isExist = true;
+            //System.out.println("시작점 : " + i + " / 값 : " + discount[i]);
+            for(int j=0;j<cnt;j++) {
+                int idx = i+j;
+
+                String idxKey = discount[idx];
+                if(!temp.containsKey(idxKey)) {
+                    isExist = false;
+                    break;
+                }
+
+                int valueCnt = temp.get(idxKey);
+                if(valueCnt-1 < 0) {
+                    isExist=false;
+                    break;
+                }
+                temp.put(idxKey,valueCnt-1);
+                //System.out.println(idxKey + " / " +temp.get(idxKey));
             }
-        if(check(number))answer++;
-        for(int i=10;i<discount.length;i++){
-            if(index.containsKey(discount[i-10])){
-            last=index.get(discount[i-10]);
-            number[last]++;
+
+            if(isExist) {
+                for(String key : map.keySet()) {
+                    if(temp.get(key) != 0) {
+                        isExist = false;
+                        break;
+                    }
+                }
             }
-            if(index.containsKey(discount[i])){
-            food=index.get(discount[i]);
-            number[food]--;
-            }
-            if(check(number))answer++;
+
+            if(isExist)
+                answer++;
         }
         return answer;
-    }
-    
-    boolean check(int[] number){
-        for(int num: number){
-            
-            if(num>0)return false;
-        }
-        return true;
     }
 }
