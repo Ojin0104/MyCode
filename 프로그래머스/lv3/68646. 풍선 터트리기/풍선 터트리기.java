9b3316a9bcc153b4d6a0b457094812d1bbf0a7b1
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-05-02 19:14:00 +0900

    [level 3] Title: 풍선 터트리기, Time: 231.48 ms, Memory: 199 MB -BaekjoonHub

--- 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,33 +1,23 @@
 import java.util.*;
 class Solution {
-    static HashMap<Integer,Boolean> map;
     public int solution(int[] a) {
-        //딱한번만 번호작은 풍선가능 -> 보통은 큰 풍선터뜨려야함
-       
-        //i 앞뒤로 최대값 비교 가운대값 i 가 가장작은 경우 양쪽 가능 나머지 세개다 가능 map에 비교
-        map=new HashMap<>();
+        int answer = 0;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        HashSet<Integer> hs = new HashSet<>();
+        // int[][] dp=new int[a.length][2];
+        for(int i=0;i<a.length;i++){
+            min1=Math.min(min1,a[i]);
+            min2=Math.min(min2,a[a.length-1-i]);
+            hs.add(min1);
+            hs.add(min2);
+            // dp[i][0]=min1;
+            // dp[a.length-1-i][1]=min2;
 
-        int[] front=new int[a.length];
-        int[] back=new int[a.length];
-        front[0]=a[0];
-        back[a.length-1]=a[a.length-1];
-        for(int i=1;i<a.length;i++){
-            front[i]=Math.min(front[i-1],a[i-1]);
         }
-        for(int i=a.length-2;i>=0;i--){
-            back[i]=Math.min(back[i+1],a[i+1]);
-        }
-        for(int i=1;i<a.length-1;i++){
-           
-            if(!(a[i]>front[i]&&a[i]>back[i])){
-                map.put(a[i],true);
-            }
-                map.put(front[i],true);
-                map.put(back[i],true);
-        }
-        
-        
-        int answer = map.size();
-        return answer;
+        // for(int i=0;i<dp.length;i++){
+        //     System.out.println(dp[i][0]+" "+dp[i][1]);
+        // }
+        return hs.size();
     }
 }
