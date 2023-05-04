6233551f053ca7404034848a872919deda4cd5d6
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-05-04 12:58:22 +0900

    [level 2] Title: 방문 길이, Time: 14.50 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
@@ -1,56 +1,42 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 class Solution {
-    static HashMap<String,Boolean> map=new HashMap<>();
-    static int[] now=new int[]{0,0};
-    static int answer=0;
     public int solution(String dirs) {
-        
-        
-        for(int i=0;i<dirs.length();i++){
-            char ch=dirs.charAt(i);
-            move(ch);
-        }
-        
-        return answer;
-    }
-    
-    static void move(char dir){
-        String start=now[0]+" "+now[1];
-        
-        if(dir=='U'){
-            now[1]+=1;
-            if(now[1]==6){
-                now[1]--;
-                return;
+        Map<String, int[]> map = new HashMap<>();
+        map.put("U", new int[] {0, 1});
+        map.put("D", new int[] {0, -1});
+        map.put("R", new int[] {1, 0});
+        map.put("L", new int[] {-1, 0});
+
+        String[] arr = dirs.split("");
+
+        Set<String> set = new HashSet<>();        
+        int cx = 0;
+        int cy = 0;
+
+        for(int i=0; i<arr.length; i++) {
+            String s = arr[i];
+
+            int x = map.get(s)[0];
+            int y = map.get(s)[1];
+
+            cx += x;
+            cy += y;
+
+            if(cx < -5 || cx > 5) {
+                cx -= x;
+                continue;
             }
-        }else if(dir=='D'){
-            now[1]-=1;
-            if(now[1]==-6){
-                now[1]++;
-                return;
+            if(cy < -5 || cy > 5) {
+                cy -= y;
+                continue;
             }
-        }else if(dir=='R'){
-            now[0]++;
-            if(now[0]==6){
-                now[0]--;
-                return;
-            }
-        }else{
-            now[0]--;
-            if(now[0]==-6){
-                now[0]++;
-                return;
-            }
-        }
-        
-        String end=now[0]+" "+now[1];
-        
-        if(!map.containsKey(start+end)){
-            answer++;
-        
-            map.put(start+end,true);
-            map.put(end+start,true);
-        }
-        return;
+            set.add(""+(cx-x)+ ""+ (cy-y)+ ""+cx + ""+cy);
+            set.add(""+cx + ""+cy+""+(cx-x)+ ""+ (cy-y));
+        } 
+        return set.size()/2;
     }
 }
