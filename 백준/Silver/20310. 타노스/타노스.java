f9149876b433f4ce1d196ebca6998a3f340796c4
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-05-05 18:30:07 +0900

    [Silver III] Title: 타노스, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        String s=br.readLine();
+        StringBuilder sb=new StringBuilder(s);
+        int zero=0;
+        int one=0;
+        for(int i=0;i<sb.length();i++){
+            if(sb.charAt(i)=='0')zero++;
+            else one++;
+        }
+        zero/=2;
+        one/=2;
+        for(int i=0;i<sb.length();i++){
+            if(sb.charAt(i)=='1') {
+                sb.setCharAt(i, '2');
+                one--;
+            }
+            if(one==0)break;
+        }
+
+        for(int i=sb.length()-1;i>=0;i--){
+            if(sb.charAt(i)=='0'){
+                sb.setCharAt(i,'2');
+                zero--;
+            }
+            if(zero==0)break;
+        }
+        
+        for(int i=sb.length()-1;i>=0;i--){
+            if(sb.charAt(i)=='2')sb.deleteCharAt(i);
+        }
+
+        System.out.println(sb);
+            }
+}
