ba5e9ac17942b0a8975edc07b6bd54d94a806a87
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-05-17 00:21:06 +0900

    [Gold V] Title: 강의실, Time: 852 ms, Memory: 53416 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        int N=Integer.parseInt(st.nextToken());
+
+        PriorityQueue<int[]> remainClass=new PriorityQueue<>(new Comparator<>(){
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0]-o2[0];
+            }
+        });
+        PriorityQueue<int[]>  nowClass=new PriorityQueue<>(new Comparator<>(){
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1]-o2[1];
+            }
+        });
+
+        for(int i=0;i<N;i++){
+            st=new StringTokenizer(br.readLine());
+
+            st.nextToken();
+            remainClass.add(new int[]{Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())});
+
+        }
+        int max=0;
+        while(!remainClass.isEmpty()){
+            int[] now=remainClass.poll();
+
+
+            while(!nowClass.isEmpty()){
+                if(nowClass.peek()[1]<=now[0]){
+                    nowClass.poll();
+                }else{
+                    break;
+                }
+            }
+            nowClass.add(now);
+            max=Math.max(max,nowClass.size());
+        }
+        System.out.println(max);
+    }
+}
