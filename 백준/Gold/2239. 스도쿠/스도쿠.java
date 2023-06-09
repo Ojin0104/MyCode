558e84b7aaca56c4e19e5aa189b3202ac440242e
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-06-09 23:58:59 +0900

    [Gold IV] Title: 스도쿠, Time: 660 ms, Memory: 19068 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 
 public class Main {
     static boolean flag=false;
+    static StringBuilder sb=new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         char[][] map=new char[9][9];
@@ -20,10 +21,11 @@ public class Main {
         if(x==9&&y==0){
             for(int i=0;i<9;i++){
                 for(int j=0;j<9;j++){
-                    System.out.print(map[i][j]);
+                    sb.append(map[i][j]);
                 }
-                System.out.println();
+                sb.append("\n");
             }
+            System.out.println(sb.toString());
             flag=true;
             return;
         }
@@ -39,25 +41,17 @@ public class Main {
         }else{
             for(int i=1;i<=9;i++){
                 if(checkLine(map,x,y,i)&&checkSquare(map,x,y,i)){
-                    char[][] nextMap=copyMap(map);
-                    nextMap[x][y]=(char)(i+'0');
-                    dfs(nextMap,next_x,next_y);
+                    
+                    map[x][y]=(char)(i+'0');
+                    dfs(map,next_x,next_y);
+                    map[x][y]='0';
                     if(flag)return;
                 }
             }
         }
     }
 
-    static char[][] copyMap(char[][] map){
-        char[][] newMap=new char[9][9];
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                newMap[i][j]=map[i][j];
-            }
-        }
-        return newMap;
-    }
-
+   
     static boolean checkLine(char[][] map,int x,int y,int num){
         char numch=(char)(num+'0');
         for(int i=0;i<9;i++){
