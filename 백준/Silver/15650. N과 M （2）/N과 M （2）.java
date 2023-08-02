657be74d02d6d55ab7f4f619e04a23f3a5fa3126
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-08-02 11:55:20 +0900

    [Silver III] Title: N과 M (2), Time: 144 ms, Memory: 15800 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,31 +4,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static StringBuilder sb=new StringBuilder();
-    static int M;
-    static int N;
+	static StringBuilder sb = new StringBuilder();
+	static int M;
+	static int N;
+	static int[] answer;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        StringTokenizer st=new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		answer = new int[M];//M 개 선택한 배열
+		dfs(1, 0, answer);
+		System.out.println(sb);
+	}
 
-       N=Integer.parseInt(st.nextToken());
-        M=Integer.parseInt(st.nextToken());
+	static void dfs(int now, int num, int[] answer) {
 
-
-        dfs(1,0,"");
-        System.out.println(sb);
-    }
-    static void dfs(int now,int num,String sbb){
-        
-        if(num==M){
-            sb.append(sbb+"\n");
-            return;
-        }
-        if(now==N+1)return;
-        
-        dfs(now+1,num+1,sbb+now+" ");
-        dfs(now+1,num,sbb);
-    }
+		if (num == M) {//M개를 선택했을 경우 sb에 넣고 return
+			for(int row=0;row<answer.length;row++) {
+				sb.append(answer[row]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		if (now == N + 1)//숫자범위 넘어갔을 경우 return
+			return;
+		answer[num]=now;
+		dfs(now + 1, num + 1, answer);//선택한 경우
+		dfs(now + 1, num, answer);//선택안한 경우
+	}
 }
