2a58867b5d3230b8171213933e002f3e613ff5eb
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-08-04 11:24:02 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 45452 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -3,22 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-
+/**
+ * Queue를 사용하는 문제
+ * @author 한영진
+ *
+ */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
         int N=Integer.parseInt(br.readLine());
 
+        
         Queue<Integer> que=new LinkedList<>();
         for(int i=1;i<=N;i++){
-            que.add(i);
+            que.add(i);//큐에 순서대로 값넣어줌
         }
 
-        while(que.size()!=1){
+        while(que.size()!=1){//한개 버리고 한개는 밑으로 넣어줌
             que.poll();
             que.add(que.poll());
         }
         System.out.println(que.poll());
     }
+	
 }
