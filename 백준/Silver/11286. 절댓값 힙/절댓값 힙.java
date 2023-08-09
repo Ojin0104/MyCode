89203c4cd79fbc2d389715db15d44eb253333dc1
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-08-09 11:59:12 +0900

    [Silver I] Title: 절댓값 힙, Time: 440 ms, Memory: 31300 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -2,32 +2,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-//람다식쓴케이스
+
+/**
+ * 컴퍼레이터에 절대값으로 값을 비교해준다.
+ * 
+ * @author 영진
+ *
+ */
 public class Main {
+	static StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int times = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->Math.abs(a)-Math.abs(b)==0?a-b:Math.abs(a)-Math.abs(b));
-		StringBuilder sb =new StringBuilder();
-		
-		
-		for(int i=0;i<times;i++) {
-			int now=Integer.parseInt(br.readLine());
-			
-			if(now==0) {
-				
-				if(pq.isEmpty()) {
-					sb.append(0+"\n");
-				}else {
-					sb.append(pq.poll()+"\n");
-				}
-			}else {
-				pq.add(now);
+		int query = Integer.parseInt(br.readLine());
+		// 절대값 오름차순으로 설정
+		PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+			if (Math.abs(a) == Math.abs(b)) {//절대값같을시엔 기존 수 오름차순
+				return a - b;
 			}
-			
+			return Math.abs(a) - Math.abs(b);
+		});
+
+		for (int index = 0; index < query; index++) {// 쿼리문 처리
+			int num = Integer.parseInt(br.readLine());
+			if (num == 0) {
+				// 출력
+				if (pq.isEmpty()) {
+					sb.append(0 + "\n");
+				} else {
+					sb.append(pq.poll() + "\n");
+				}
+			} else {
+				// 입력
+				pq.add(num);
+			}
+
 		}
-		
 		System.out.println(sb);
-		
 	}
+
 }
