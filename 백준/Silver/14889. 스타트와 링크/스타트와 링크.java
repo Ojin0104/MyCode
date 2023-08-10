4cf6051fa062be0b1bdadf0f3a38208475bfb1f7
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-08-10 14:52:58 +0900

    [Silver II] Title: 스타트와 링크, Time: 204 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,60 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * 
+ * @author 영진
+ * 1. 스타트팀과 링크팀으로 나눈 조합두개를 찾는다. 
+ * 2. 각 팀의 조합점수를 더해준다.
+ *
+ */
 public class Main {
-    static int N;
-    static int[][] point;
-    static int min=Integer.MAX_VALUE;
-    static boolean[] visit;
-    public static void main(String args[]) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        N=Integer.parseInt(br.readLine());
-        point=new int[N+1][N+1];
-        StringTokenizer st;
-        visit=new boolean[N+1];
-        for(int i=1;i<N+1;i++){
-            st=new StringTokenizer(br.readLine());
-            for(int j=1;j<N+1;j++){
-
-                point[i][j]=Integer.parseInt(st.nextToken());
-            }
-        }
-        dfs(0,0);
-        System.out.println(min);
-    }
-    static void dfs(int order,int n){
-        if(n==N/2){
-            min=Math.min(sums(),min);
-        }
-        if(order>=N)return;
-        order++;
-        n++;
-        visit[order]=true;
-        dfs(order,n);
-        n--;
-        visit[order]=false;
-        dfs(order,n);
-    }
-    static int sums(){
-//        for(int i=1;i<visit.length;i++){
-//            System.out.print(visit[i]);
-//        }
-//        System.out.println();
-        int start=0;
-        int link=0;
-        for(int i=1;i<N+1;i++){
-            for(int j=i+1;j<N+1;j++){
-                if(visit[i]&&visit[j]){
-                    start+=point[i][j]+point[j][i];
-                }else if(!visit[i]&&!visit[j]){
-                    link+=point[i][j]+point[j][i];
-                }
-            }
-        }
-       // System.out.println(start+" "+link);
-
-        return Math.abs(start-link);
-    }
+	static int min = Integer.MAX_VALUE;
+	static int[][] talent;
+	static int N;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		talent = new int[N][N];
+		
+		for(int row =0;row<N;row++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int col = 0 ;col<N;col++) {
+				talent[row][col]= Integer.parseInt(st.nextToken());
+			}
+		}
+		int[] start= new int[N/2];
+		int[] link= new int[N/2];
+		
+		dfs(start,link,0,0,0) ;
+			
+		System.out.println(min);
+		
+	}
+	
+	static void dfs(int[] start,int[] link,int left,int right,int index) {
+		if(left+right==N) {//두팀 모두 정해졌을때
+			//두팀의 차이 구하기
+			min= Math.min(min,compareTeam(start,link));
+			return;
+		}
+		
+		if(left<N/2) {//팀에 자리가있을때만 로직 수행
+			start[left]=index;
+			dfs(start,link,left+1,right,index+1);
+		}
+		
+		if(right<N/2) {
+			link[right]=index;
+			dfs(start,link,left,right+1,index+1);
+		}
+	}
+	
+	static int compareTeam(int[] start, int[] link) {
+		int startSum=0;
+		int linkSum=0;
+		
+		for(int l=0;l<N/2;l++) {//각팀 2명씩 조합하여 sum을 구해준다.
+			for(int r=l+1;r<N/2;r++) {
+				startSum+=talent[start[l]][start[r]]+talent[start[r]][start[l]];
+				linkSum+=talent[link[l]][link[r]]+talent[link[r]][link[l]];
+			}
+		}
+		
+		return Math.abs(startSum-linkSum);
+	}
 }
