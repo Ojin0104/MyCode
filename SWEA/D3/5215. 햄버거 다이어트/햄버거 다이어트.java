193b3cd68c72aa07e07c602d41003e929e82bab2
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-08-10 13:43:02 +0900

    [D3] Title: 햄버거 다이어트, Time: 142 ms, Memory: 22,964 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,53 +1,56 @@
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+/**
+ * dp풀이방식 점화식 i칼로리일때 가장 큰 맛점수를 기록한다. dp[i] 과dp[i-재료칼로리]+재료맛 비교하여 큰점수를 기록한다.
+ * 뒤에서부터 갱신을하는데 만약 300cal에 flavor 50이라면 300~limitCal까지 50으로 채워줘야한다. 만약 앞에서 부터
+ * 갱신을 한다면 현재 갱신된 값이 뒤에까지 영향을 미치게 되므로 뒤에서부터 갱신!!
+ * 
+ * @author 영진
+ *
  */
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+public class Solution {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T;
-		T=Integer.parseInt(br.readLine());
-		StringBuilder sb=new StringBuilder();
-        
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			StringTokenizer st=new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int L = Integer.parseInt(st.nextToken());
-            
-            ingredient[] ingredients=new ingredient[N];
-            for(int i=0;i<N;i++){
-            	st=new StringTokenizer(br.readLine());
-                int flavor=Integer.parseInt(st.nextToken());
-                int calorie=Integer.parseInt(st.nextToken());
-                ingredients[i]=new ingredient(flavor,calorie);
-            }
-            //Arrays.sort(ingredients,(a,b)->a.calorie-b.calorie);
-            
-            int[] dp=new int[L+1];// i칼로리일때 가장 큰 맛점수 기록
-            
-            for(int i=0;i<N;i++){
-                for(int j=L;j>=ingredients[i].calorie;j--){
-                    dp[j]=Math.max(dp[j],dp[j-ingredients[i].calorie]+ingredients[i].flavor);
-                }
-            }
-            sb.append("#"+test_case+" "+dp[L]+"\n");
+		T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int limitCal = Integer.parseInt(st.nextToken());
+
+			ingredient[] ingredients = new ingredient[N];
+			for (int index = 0; index < N; index++) {
+				st = new StringTokenizer(br.readLine());
+				int flavor = Integer.parseInt(st.nextToken());
+				int calorie = Integer.parseInt(st.nextToken());
+				ingredients[index] = new ingredient(flavor, calorie);
+			}
+
+			int[] dp = new int[limitCal + 1];// i칼로리일때 가장 큰 맛점수 기록
+
+			for (int index = 0; index < N; index++) {
+				for (int cal = limitCal; cal >= ingredients[index].calorie; cal--) {// 최대 칼로리 부터 앞으로가면서 갱신
+					dp[cal] = Math.max(dp[cal], dp[cal - ingredients[index].calorie] + ingredients[index].flavor);
+				}
+			}
+			sb.append("#" + test_case + " " + dp[limitCal] + "\n");
 		}
-        System.out.println(sb);
+		System.out.println(sb);
 	}
-    
-     static class ingredient{//맛과 칼로리를 저장하는 class
-        int flavor;
-        int calorie;
-        public ingredient(int flavor,int calorie){
-            this.flavor=flavor;
-            this.calorie=calorie;
-        }
-     }
+
+	static class ingredient {// 맛과 칼로리를 저장하는 class
+		int flavor;
+		int calorie;
+
+		public ingredient(int flavor, int calorie) {
+			this.flavor = flavor;
+			this.calorie = calorie;
+		}
+	}
 }
+
