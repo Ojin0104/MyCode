96a4bc40af3d87d02caa5b0e7e958182927c90e0
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-08-13 11:12:09 +0900

    [unrated] Title: 요격 시스템, Time: 0.45 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -2,41 +2,26 @@ import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
-        //현재범위 우선순위큐와 아직 범위전 배열을 둠
         
-        Arrays.sort(targets,(a,b)->{
-            if(a[0]==b[0]){
-                return a[1]-b[1];
-            }else{
-                return a[0]-b[0];
-            }
-            });
+        Arrays.sort(targets,(a,b)->(a[0]-b[0]));
         
-        int end =0;
+        int end =targets[0][1];
         
-        for(int i=0;i<targets.length;i++){
+        for(int i=1;i<targets.length;i++){
             int next_start=targets[i][0];
             int next_end= targets[i][1];
-            if(end==0){
-                end = next_end;
-                continue;
-            }
             
-            if(next_start>=end){
+            if(next_start>=end){//다음시작값이 end보다 크거나 같으면 이전의 미사일들 요격
                 answer++;
                 end=next_end;
-
-            }else if(next_end<end){
+            }else if(next_end<end){//다음end값이 더 작다면 end값 앞으로 떙겨줌(end앞으로 땡겨도 start보다는 크므로 같이 요격가능)
                 end=next_end;
             }
-            
-            
         }
        
-        
         if(end!=0){//아직 요격할 미사일이 남아있는 경우
-            answer++;
-        }
+             answer++;
+         }
         return answer;
     }
 }
