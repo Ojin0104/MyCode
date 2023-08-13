f28a36c39308cf389d341e64101730002880e32d
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-08-13 23:14:47 +0900

    [Bronze II] Title: 3대 측정, Time: 836 ms, Memory: 166628 KB -BaekjoonHub

--- 백준/Bronze/20299. 3대 측정/3대 측정.java
+++ 백준/Bronze/20299. 3대 측정/3대 측정.java
@@ -6,49 +6,48 @@ import java.util.StringTokenizer;
 /**
  * 1.최소 점수 못넘을시 바로 continue;
  * 2.최소 및 몇균 넘을시 바로 sb추가
- *
  */
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
-        StringBuilder sb =new StringBuilder();//팀수 저장
-        StringBuilder teamRatings= new StringBuilder(); //인원저장
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder teamRatings = new StringBuilder(); //인원저장
 
-        int teams= Integer.parseInt(st.nextToken());
+        int teams = Integer.parseInt(st.nextToken());
         int total = Integer.parseInt(st.nextToken());
         int limit = Integer.parseInt(st.nextToken());
-        int[] team= new int[3];
-        int count=0;
+        int[] team = new int[3];
+        int count = 0;
 
-        for(int no=0;no<teams;no++){
-            int teamSum=0;
-            boolean flag =true;
+        for (int no = 0; no < teams; no++) {
+            int teamSum = 0;
+            boolean flag = true;
             st = new StringTokenizer(br.readLine());
 
-            for(int index=0;index<3;index++){
-                team[index]=Integer.parseInt(st.nextToken());
+            for (int index = 0; index < 3; index++) {
+                team[index] = Integer.parseInt(st.nextToken());
 
-                if(team[index]>=limit){
-                    teamSum+=team[index];
-                }else{
-                    flag=false;
+                if (team[index] >= limit) {
+                    teamSum += team[index];
+                } else {
+                    flag = false;
                     break;
                 }
             }
 
-            if(flag&&teamSum>=total){
+            if (flag && teamSum >= total) {
                 count++;
-                addStr(teamRatings,team);
+                addStr(teamRatings, team);
             }
         }
-        System.out.println(sb.append(count+"\n").append(teamRatings));
+        System.out.println(count);
+        System.out.println(teamRatings);
     }
 
-    static void addStr(StringBuilder teamRatings,int[] team){//가입가능 팀원 점수저장
-        for(int rating: team){
-            teamRatings.append(rating+" ");
+    static void addStr(StringBuilder teamRatings, int[] team) {//가입가능 팀원 점수저장
+        for (int rating : team) {
+            teamRatings.append(rating + " ");
         }
     }
 }
