d9d2c8be60e6d3f72dcb5590683016703372f563
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-08-16 17:58:06 +0900

    [Gold IV] Title: 알파벳, Time: 884 ms, Memory: 12416 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,52 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
+/**
+ * 
+ * @author SSAFY
+ *  1.알파벳 대문자는 총 26개 이므로 26사이즈의 배열을 만들어 체크를 한다.
+ *  2.dfs를 통해 이동할 수 있을때 까지 칸을 계산한다.
+ *  3.dp 까지 사용해야하나
+ */
 public class Main {
-    static String[] map;
-    static int max=0;
-    static int R;
-    static int C;
-    static int[] dx={0,1,-1,0};
-    static int[] dy={1,0,0,-1};
-    public static void main(String args[]) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static boolean[] alpha = new boolean[26];
+	static int answer = 0;
+	static int[] dr = { 1, 0, -1, 0 };
+	static int[] dc = { 0, 1, 0, -1 };
+	static int R;
+	static int C;
 
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        R=Integer.parseInt(st.nextToken());
-        C=Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        map=new String[R];
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
 
-        for(int i=0;i<R;i++){
+		char[][] map = new char[R][C];
 
-            map[i]=br.readLine();
-        }
-        boolean[] check=new boolean[26];
-        check[map[0].charAt(0)-'A']=true;
-        dfs(check,0,0,0);
-        System.out.println(max);
-    }
-    static void dfs(boolean[] check,int x, int y,int num){
+		for (int row = 0; row < R; row++) {
+			String line = br.readLine();
+			for (int col = 0; col < C; col++) {
+				map[row][col] = line.charAt(col);
+			}
+		}
+		alpha[map[0][0] - 'A'] = true;
+		dfs(map, 0, 0, 1);
 
+		System.out.println(answer);
+	}
 
-        int next_x;
-        int next_y;
-        for(int i=0;i<4;i++){
-            next_x=x+dx[i];
-            next_y=y+dy[i];
-            if(next_x<0||next_x>=R||next_y<0||next_y>=C)continue;
+	static void dfs(char[][] map, int row, int col, int count) {
+		answer = Math.max(count, answer);
 
-            if(check[(map[next_x].charAt(next_y)-'A')]){
-                max=Math.max(max,num+1);
-                continue;
-            }
-            check[(map[next_x].charAt(next_y)-'A')]=true;
-            dfs(check,next_x,next_y,num+1);
-            check[(map[next_x].charAt(next_y)-'A')]=false;
-        }
+		for (int dir = 0; dir < 4; dir++) {
+			int next_r = row + dr[dir];
+			int next_c = col + dc[dir];
 
-    }
+			if (next_r < 0 || next_c < 0 || next_r >= R || next_c >= C)
+				continue;
+
+			if (alpha[map[next_r][next_c] - 'A'])
+				continue;
+
+			alpha[map[next_r][next_c] - 'A'] = true;
+			dfs(map, next_r, next_c, count + 1);
+			alpha[map[next_r][next_c] - 'A'] = false;
+		}
+	}
 }
