de4c48a67d7365c0d2b8dd58107d842d1379b575
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-08-22 13:15:31 +0900

    [D4] Title: 서로소 집합, Time: 700 ms, Memory: 105,912 KB -BaekjoonHub

---
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * @author 영진
+ * 1.union-find문제
+ * 
+ */
+public class Solution {
+	static int[] parent;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb =new StringBuilder();
+		
+		for(int test_case=1;test_case<=T;test_case++) {
+			StringTokenizer st =new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			parent = new int[N+1];
+			
+			for(int idx=1;idx<N+1;idx++) {//parent배열 초기화
+				parent[idx]=idx;
+			}
+			
+			sb.append("#"+test_case+" ");
+			
+			for(int query=0;query<M;query++) {
+				st = new StringTokenizer(br.readLine());
+				
+				if(Integer.parseInt(st.nextToken())==0) {
+					//union
+					int nodeA=Integer.parseInt(st.nextToken());
+					int nodeB=Integer.parseInt(st.nextToken());
+					
+					union(nodeA,nodeB);
+				}else {
+					int nodeA=Integer.parseInt(st.nextToken());
+					int nodeB=Integer.parseInt(st.nextToken());
+					
+					//find 비교
+					if(find(nodeA)==find(nodeB)) {
+						sb.append("1");
+					}else {
+						sb.append("0");
+					}
+				}
+			}
+			sb.append("\n");
+			
+		}
+		System.out.println(sb);
+	}
+	
+	public static int find(int a) {
+		if(parent[a]==a) {
+			return a;
+		}
+		
+		return parent[a]=find(parent[a]);
+	}
+	
+	public static void union(int a, int b) {
+		int A = find(a);
+		int B = find(b);
+		
+		if(A==B) {
+			//이미 합집합
+			return;
+		}
+		
+		if(A<B) {
+			parent[B]=A;
+		}else {
+			parent[A]=B;
+		}
+		
+	}
+}
