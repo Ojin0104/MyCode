44f3ee7022d57db8a7141e6c7aaea80e4f1c5d45
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-08-25 13:34:40 +0900

    [Gold IV] Title: 최단경로, Time: 828 ms, Memory: 121912 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
@@ -22,6 +24,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 
@@ -54,7 +57,9 @@ public class Main {
 				sb.append(shortestDist[idx] + "\n");
 			}
 		}
-		System.out.println(sb);
+		bw.write(sb.toString());
+		bw.close();
+		//System.out.println(sb);
 	}
 
 	static void dikstra(int start) {
