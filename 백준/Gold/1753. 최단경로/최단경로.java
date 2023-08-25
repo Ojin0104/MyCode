9ca65ea0608ae863a9a30f6357009526ec721c85
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2023-08-25 13:32:59 +0900

    [Gold IV] Title: 최단경로, Time: 832 ms, Memory: 121996 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -8,10 +8,12 @@ import java.util.StringTokenizer;
 
 /**
  * 
- * @author 영진 1.다잌스트라 문제 2. 한점을 기준으로 연결된 경로 가중치와 연결되지 않았아면 INF로 배열을 만든다. 3.
- *         우선순위큐에 한점에서 다른점으로 가는 가중치를 저장해두고 가중치가 낮은 순서로 정렬한다. 4. 큐에 값이 없을떄까지 큐에서
- *         값을빼(한점에서 해당점으로 갈수 있는 가장 낮은 가중치) 해당 노드에서 다른노드를 갈때 현재보다 빠르게 갈 수 있는지
- *         체크한다. 5. 만약 빠르게 갈 수 있다면 우선순위 큐에 넣어준다
+ * @author 영진
+ * 1.다잌스트라 문제 
+ * 2. 한점을 기준으로 연결된 경로 가중치와 연결되지 않았아면 INF로 배열을 만든다. 
+ * 3. 우선순위큐에 한점에서 다른점으로 가는 가중치를 저장해두고 가중치가 낮은 순서로 정렬한다. 
+ * 4. 큐에 값이 없을떄까지 큐에서 값을빼(한점에서 해당점으로 갈수 있는 가장 낮은 가중치) 해당 노드에서 다른노드를 갈때 현재보다 빠르게 갈 수 있는지 체크한다. 
+ * 5. 만약 빠르게 갈 수 있다면 우선순위 큐에 넣어준다
  */
 public class Main {
 	static ArrayList<Edge>[] graph;
@@ -57,23 +59,25 @@ public class Main {
 
 	static void dikstra(int start) {
 		PriorityQueue<Edge> pq = new PriorityQueue<>((e1, e2) -> e1.dist - e2.dist);// 거리가까운순
-		boolean[] check = new boolean[V + 1];
+		//boolean[] check = new boolean[V + 1];
 		pq.add(new Edge(start, 0));
 		Arrays.fill(shortestDist, Integer.MAX_VALUE);
 		shortestDist[start] = 0;
-
+		//int size=0;
 		while (!pq.isEmpty()) {
 			Edge now = pq.poll();
-			if(check[now.node])continue;
-			
-			check[now.node] = true;
+			if(now.dist>shortestDist[now.node])continue;
+			//if(check[now.node])continue;
+			//size++;
+			//if(size==V)return;
+			//check[now.node] = true;
 			//shortestDist[now.node] = now.dist;
 			for (Edge next : graph[now.node]) {
-				if (check[next.node])
-					continue;
+			//	if (check[next.node])
+				//	continue;
 				if (shortestDist[next.node] > shortestDist[now.node] + next.dist) {
 					shortestDist[next.node] = shortestDist[now.node] + next.dist;
-					pq.add(new Edge(next.node, shortestDist[next.node]));
+					pq.offer(new Edge(next.node, shortestDist[next.node]));
 				}
 			}
 		}
