cd7b523b1983f59db70364688a8bfc2b8f5d92cc
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2024-03-07 22:55:35 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 7655.74 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,59 +1,44 @@
-import java.util.*;
-class Solution {
-    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        int[] answer = new int[enroll.length];
-        
-        HashMap<String,Node> map = new HashMap<>();
-        Node head = new Node(null,0);
-        map.put("-",head);
-        
-        //트리와 맵에 직원 등록
-        enrollSeller(enroll,referral,map);
-        
-        for(int idx = 0;idx<seller.length;idx++){
-            Node now = map.get(seller[idx]);
-            
-            now.sellItem(amount[idx]*100);
-        }
-        
-        for(int idx= 0 ;idx<enroll.length;idx++){
-           answer[idx] = map.get(enroll[idx]).money;
-        }
-        return answer;
-    }
-    
-    static void enrollSeller(String[] enroll, String[] referral,HashMap<String,Node> map){
-        for(int i =0 ;i<enroll.length;i++){
-            Node parent = map.get(referral[i]);
-            Node me = new Node(parent,0);
-            map.put(enroll[i],me);
-            parent.addChild(me);
-        }
-    }
-    
-    
-}
+import java.util.HashMap;
 
-class Node{
-    Node parent;
-    ArrayList<Node> children = new ArrayList<>();
+class Person {
+    String name;
+    Person parent;
     int money;
-    public Node(Node parent,int money){
+
+    public Person(String name, Person parent, int money) {
+        this.name = name;
         this.parent = parent;
         this.money = money;
     }
-    
-    void sellItem(int amount){
-        if(parent!=null){
-            int next = amount/10;
-            this.money += amount-next;
-            parent.sellItem(next);
+
+    void getReward(int i) {
+        int moneyToParent = (int) (i * 0.1);
+        this.money += i - moneyToParent;
+        if (this.parent != null)
+            this.parent.getReward(moneyToParent);
+    }
+}
+
+class Solution {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        HashMap<String, Person> personHashMap = new HashMap<>();
+        for (String name : enroll)
+            personHashMap.put(name, new Person(name, null, 0));
+
+        for (int i = 0; i < enroll.length; i++) {
+            if (referral[i].equals("-"))
+                continue;
+            personHashMap.get(enroll[i]).parent = personHashMap.get(referral[i]);
         }
+
+        for (int i = 0; i < seller.length; i++)
+            personHashMap.get(seller[i]).getReward(amount[i] * 100);
+
+        int[] result = new int[enroll.length];
+
+        for (int i = 0; i < result.length; i++)
+            result[i] = personHashMap.get(enroll[i]).money;
+
+        return result;
     }
-    
-    void addChild(Node child){
-        this.children.add(child);
-    }
-    
-    
 }
