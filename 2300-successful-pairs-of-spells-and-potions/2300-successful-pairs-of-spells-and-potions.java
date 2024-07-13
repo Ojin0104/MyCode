baa6fe71cba6926a6d2c3162754818da1b36ded6
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2024-07-13 14:59:58 +0900

    Time: 41 ms (57.06%), Space: 64.4 MB (52.13%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+
+        int[] answer = new int[spells.length];
+
+        for(int idx = 0 ;idx<spells.length; idx++){
+            answer[idx]=potions.length-binSearch(spells[idx],success,potions);
+        }
+
+        return answer;
+    }
+
+
+    int binSearch(int spell,long num,int[] potions){
+        int left = 0;
+        int right = potions.length-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if((long)potions[mid]*spell<num){
+                left= mid+1;
+            }else{
+                right= mid -1;
+            }
+        }
+       // System.out.println(right);
+        return right+1;
+    }
+}
