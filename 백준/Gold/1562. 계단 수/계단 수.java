a4b4135233fa40eaf34ab224f8a3fe2d285117e2
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2024-08-28 22:21:06 +0900

    [Gold I] Title: 계단 수, Time: 104 ms, Memory: 18532 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.java
+++ 백준/Gold/1562. 계단 수/계단 수.java
@@ -1,37 +1,46 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     static int mod = 1000000000;
-    static int[][][] dp = new int[101][10][1 << 10];
+    static int N;
+    static Integer[][][] dp;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        sc.close();
+        N = sc.nextInt();
+        dp = new Integer[N + 1][10][1 << 10];
+        int result = 0;
 
         for (int i = 1; i <= 9; i++) {
-            dp[1][i][1 << i] = 1;
+            result = (result + find(N, i, 1 << i)) % mod;
         }
 
-        for (int i = 2; i <= N; i++) {
-            for (int j = 0; j <= 9; j++) {
-                for (int k = 0; k < (1 << 10); k++) {
-                    int temp = k | (1 << j);
-                    if (j == 0) {
-                        dp[i][j][temp] = (dp[i][j][temp] + dp[i - 1][1][k]) % mod;
-                    } else if (j == 9) {
-                        dp[i][j][temp] = (dp[i][j][temp] + dp[i - 1][8][k]) % mod;
-                    } else {
-                        dp[i][j][temp] = ((dp[i][j][temp] + dp[i - 1][j - 1][k]) % mod + dp[i - 1][j + 1][k]) % mod;
-                    }
-                }
+        System.out.println(result);
+    }
+
+    static int find(int digit, int num, int bit) {
+        if (digit == 1) {
+            if (bit == (1 << 10) - 1) {
+                return 1;
+            } else {
+                return 0;
             }
         }
 
-        int answer = 0;
-        for (int i = 0; i <= 9; i++) {
-            answer = (answer + dp[N][i][(1 << 10) - 1]) % mod;
+        if (dp[digit][num][bit] != null) {
+            return dp[digit][num][bit];
         }
 
-        System.out.println(answer);
+        int tmp = 0;
+
+        if (num - 1 >= 0) {
+            tmp = (tmp + find(digit - 1, num - 1, bit | 1 << (num - 1))) % mod;
+        }
+
+        if (num + 1 <= 9) {
+            tmp = (tmp + find(digit - 1, num + 1, bit | 1 << (num + 1))) % mod;
+        }
+
+        return dp[digit][num][bit] = tmp;
     }
 }
