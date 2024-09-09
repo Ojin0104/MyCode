c2f2a69be616a0459c569bcace5ac3756c398f24
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2024-09-10 01:03:08 +0900

    [level 3] Title: 매칭 점수, Time: 29.57 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/3/42893. 매칭 점수/매칭 점수.java
+++ 프로그래머스/3/42893. 매칭 점수/매칭 점수.java
@@ -1,90 +1,113 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 class Solution {
-    private HashMap<String, Page> map = new HashMap<>();
-
     public int solution(String word, String[] pages) {
-        int idx = 0;
-        for (String html : pages) {
-            Page page = new Page(idx++, html.toLowerCase());
-            page.setDefaultScore(word.toLowerCase());
-            page.setLinkCount();
-            map.put(page.url, page);
+        int answer = 0;
+       
+        
+        HashMap<String,Page> pageMap = new HashMap<>();
+        ArrayList<String> realPages = new ArrayList<>();
+        for(int idx =0 ;idx< pages.length; idx++){
+            String page = pages[idx];
+            //현재 페이지 url 정보
+            String url = findNowPage(page);
+        //각 페이지 기본점수 계산하기
+            Double point = calcNormalScore(page,word);
+        //각 페이지 href 태그 갯수 계산하기
+            List<String> hrefs = findHref(page);
+            
+            int linkCount = hrefs.size();
+            System.out.println(linkCount);
+            realPages.add(url);
+            Page nowPage = new Page(url,point,linkCount);
+            pageMap.put(url,nowPage);
         }
-
-        for (Page page : map.values()) {
-            page.setLinkScore();
+        
+        for(int idx = 0; idx<pages.length; idx++){
+            //href 페이지 url로 링크점수 가져오기
+            List<String> hrefs = findHref(pages[idx]);
+            
+            Page page =  pageMap.get(realPages.get(idx));
+            System.out.println("now "+page.url);
+            
+            if(hrefs.size()==0)continue;
+            Double linkScore = (Double)page.score/page.link;
+            for(String href : hrefs){
+                
+                if(!pageMap.containsKey(href))continue;
+                System.out.println("href "+href);
+                pageMap.get(href).pluslinkScore(linkScore);
+            }
+            
+           
         }
-
-        ArrayList<Page> list = new ArrayList(map.values());
-        Collections.sort(list);
-
-        return list.get(0).idx;
+        Double max = -1.0;
+         int maxIdx = -1;
+        for(int idx =0 ;idx<pages.length;idx++){
+            String url = realPages.get(idx);
+            System.out.println(pageMap.get(url).score+pageMap.get(url).linkScore);
+            if(pageMap.get(url).score+pageMap.get(url).linkScore>max){
+                max = pageMap.get(url).score+pageMap.get(url).linkScore;
+                maxIdx = idx;
+            }
+        }
+        
+        return maxIdx;
     }
+    
+    public List<String> findHref(String page){
+         String regex = "<a href=\"(?<link>[^\"]*)\"";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(page);
+        
+        List<String> answer = new ArrayList<>();
+        while(matcher.find()){
+           answer.add(matcher.group(1));
+        }
+       return answer;
+    }
+    
+    public Double calcNormalScore(String page,String find){
 
-    class Page implements Comparable<Page> {
-        int idx;
-        int defaultScore = 0;
-        int linkCount = 0;
-        double linkScore = 0;
-        String html, url;
+        page = page.toLowerCase();
+        find = find.toLowerCase();
 
-        Page(int idx, String html) {
-            this.idx = idx;
-            this.html = html;
-            findUrl();
+        
+        String[] words = page.split("[^a-z]+");
+         int answer = 0;
+        for(String word: words){
+            if(word.equals(find))answer++;
         }
 
-        private void findUrl() {
-            Pattern pattern = Pattern.compile("<meta property=\"og:url\" content=\"https://(.+?)\"/>");
-            Matcher matcher = pattern.matcher(html);
-            while (matcher.find()) {
-                url = matcher.group(1);
-            }
+        return (double)answer ;
+    }
+    public String findNowPage(String page){
+        String regex = "<head>[\\s\\S]*<meta[^>]*content=\"(?<url>[^\"]*)\"";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(page);
+        if(matcher.find()){
+            return matcher.group(1);
         }
-
-        public void setDefaultScore(String word) {
-            int idx = html.indexOf(word);
-            while (idx != -1) {
-                char pre = html.charAt(idx - 1);
-                char post = html.charAt(idx + word.length());
-
-                if (!Character.isLowerCase(pre) && !Character.isLowerCase(post)) {
-                    defaultScore++;
-                }
-                idx = html.indexOf(word, idx + 1);
-            }
+        return "";
+    }
+    
+    static class Page{
+        String url;
+        Double score;
+        int link;
+        Double linkScore;
+        
+        
+        public Page(String url, Double score, int link){
+            this.url = url;
+            this.score =score;
+            this.link = link;
+            this.linkScore=0.0;
         }
-
-        public void setLinkCount() {
-            int idx = html.indexOf("<a href=");
-            while (idx != -1) {
-                linkCount++;
-                idx = html.indexOf("<a href=", idx + 1);
-            }
-        }
-
-        public void setLinkScore() {
-            Pattern pattern = Pattern.compile("<a href=\"https://(.+?)\">");
-            Matcher matcher = pattern.matcher(html);
-            while (matcher.find()) {
-                String externalUrl = matcher.group(1);
-                if (map.containsKey(externalUrl)) {
-                    map.get(externalUrl).linkScore += (double) defaultScore / linkCount;
-                }
-            }
-        }
-
-        @Override
-        public int compareTo(Page other) {
-            double a = (double) this.defaultScore + this.linkScore;
-            double b = (double) other.defaultScore + other.linkScore;
-
-            return Double.compare(b, a);
+        
+        public void pluslinkScore(Double links){
+            this.linkScore += links;
         }
     }
 }
