72c1e8623fc5b626c49fb12f528b73131c78558f
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2024-11-04 21:56:38 +0900

    Time: 5 ms (64.49%), Space: 41.3 MB (97.58%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()+1];
+
+        dp[0]= true; 
+        for(int idx =1; idx<s.length()+1; idx++){
+            
+            for(int wordIdx = 0 ;wordIdx<wordDict.size(); wordIdx++){
+                String word = wordDict.get(wordIdx);
+                if(idx<word.length())continue;
+
+
+                if(dp[idx-word.length()] && checkEqual(s,idx, word)){
+                    dp[idx] = true;
+                }
+
+            }
+        }
+
+        return dp[s.length()];
+    }
+
+    static boolean checkEqual(String s, int idx, String word){
+        for(int num =0 ;num<word.length(); num++){
+            if(s.charAt(idx-word.length()+num)!=word.charAt(num))return false;
+        }
+
+        return true;
+    }
+}
