51b83f063182361f198013f2d7cc089965062e98
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2024-11-28 17:51:48 +0900

    Time: 3 ms (100%), Space: 43.8 MB (49.87%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
@@ -1,17 +1,16 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
         // left와 right 이진법 자릿 수를 left 기준으로 맞춤 
-            if(left==right) return left;
-        if(left==0)return 0;
-
+        
+     
         int leftSize = 0;
         int rightSize = 0;
         while((left>>leftSize)>0){
             leftSize++;
         }
-            while((right>>rightSize)>0){
-                        rightSize++;
-                    }
+        while((right>>rightSize)>0){
+            rightSize++;
+        }
         if(rightSize>leftSize) return 0;
 
         int answer = 0;
