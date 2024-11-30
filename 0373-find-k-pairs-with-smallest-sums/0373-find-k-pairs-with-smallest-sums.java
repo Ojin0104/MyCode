51957d1f1e6620b788534b34704c0a6e5d17386e
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2024-11-30 17:59:43 +0900

    Time: 45 ms (36.88%), Space: 59.9 MB (51.68%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -4,10 +4,10 @@ class Solution {
         int[] index = new int[nums1.length];
         PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2)-> {
 
-            if(o1.first+o1.second >o2.first + o2.second){
+            if(o1.sum >o2.sum){
                 return 1;
-            }else if(o1.first+o1.second == o2.first+o2.second){
-                if(o1.first>o2.first){
+            }else if(o1.sum == o2.sum){
+                if(o1.idx>o2.idx){
                     return 1;
                 }else{
                     return -1;
@@ -16,35 +16,30 @@ class Solution {
                 return -1;
             }
         });
-        while(true){
-            long before = Long.MAX_VALUE;
-            for(int idx = 0; idx< nums1.length; idx++){
-                int offset = index[idx];
-                if(offset== nums2.length)continue;
-                long now = nums1[idx] + nums2[offset];
-                if(now<=before){
-                    before = now;
-                    pq.add(new Node(nums1[idx],nums2[offset]));
-                    index[idx]++;
-                }else{break;}
-            }
             
-                Node node = pq.poll();
-                answer.add(List.of(node.first,node.second));
-                if(answer.size()==k)return answer;
-            
+        for(int idx = 0; idx< nums1.length; idx++){
+            pq.add(new Node(nums1[idx]+nums2[0],idx));
+        }
+
+        for(int idx = 0 ;idx < k ; idx++){
+            Node now = pq.poll();
+            answer.add(List.of(nums1[now.idx],nums2[index[now.idx]]));
+            index[now.idx]++;
+            if(index[now.idx]==nums2.length)continue;
+            pq.add(new Node(nums1[now.idx]+nums2[index[now.idx]],now.idx));
 
         }
+
+        return answer;
 
     }
 
     static class Node{
-        int first;
-        int second;
-
-        public Node(int first, int second){
-            this.first = first;
-            this.second = second;
+        int sum;
+        int idx;
+        public Node(int sum, int idx){
+            this.sum = sum;
+            this.idx = idx;
         }
     }
 }
