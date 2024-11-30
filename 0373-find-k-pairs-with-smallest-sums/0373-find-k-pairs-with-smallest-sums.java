4c7da62c3f7b96cdd58731b0295b31f141792c57
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2024-11-30 18:01:42 +0900

    Time: 29 ms (83.47%), Space: 60.2 MB (48.19%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -4,7 +4,6 @@ class Solution {
         int[] index = new int[nums1.length];
         PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2)-> o1.sum-o2.sum);
       
-            
         for(int idx = 0; idx< nums1.length; idx++){
             pq.add(new Node(nums1[idx]+nums2[0],idx));
         }
@@ -13,13 +12,11 @@ class Solution {
             Node now = pq.poll();
             answer.add(List.of(nums1[now.idx],nums2[index[now.idx]]));
             index[now.idx]++;
-            if(index[now.idx]==nums2.length)continue;
-            pq.add(new Node(nums1[now.idx]+nums2[index[now.idx]],now.idx));
-
+            if(index[now.idx]!=nums2.length)
+                pq.add(new Node(nums1[now.idx]+nums2[index[now.idx]],now.idx));
         }
 
         return answer;
-
     }
 
     static class Node{
