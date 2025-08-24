13ccd4f74022bf6ddf269bac441c242251e1fb15
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2025-08-24 15:52:10 +0900

    Time: 7 ms (26.78%), Space: 45.6 MB (87.89%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -6,24 +6,9 @@ class Solution {
         ArrayDeque<Integer> que = new ArrayDeque<>();
         int[] count = new int[numCourses];
 
-        for(int idx = 0 ; idx<numCourses; idx++){
-            graph.add(new ArrayList<>());
-        }
-        for(int[] prerequisite : prerequisites){
-            int first = prerequisite[1];
-            int second = prerequisite[0];
-            count[second]++;
-            graph.get(first).add(second);
-        }
-
-        for(int idx = 0; idx<numCourses; idx++){
-            if(count[idx] ==0 ){
-                que.addLast(idx);
-            }
-        }
-
-        int order = 0;
-
+        init(numCourses, graph, prerequisites, count);
+        
+        addQueue(que,count);
         ArrayList<Integer> answerArray = new ArrayList<>();
 
 
@@ -40,12 +25,31 @@ class Solution {
                 if(count[next]==0){
                     que.addLast(next);
                 }
-
-                
             }
         }
         if(answerArray.size()<numCourses)return new int[0];
         int[] answer = answerArray.stream().mapToInt(Integer::intValue).toArray();
         return answer;
+    }
+
+    public void addQueue(ArrayDeque<Integer> que, int[] count){
+        for(int idx = 0; idx<count.length; idx++){
+            if(count[idx] ==0 ){
+                que.addLast(idx);
+            }
+        }
+    }
+
+    public void init(int numCourses, List<List<Integer>> graph, int[][] prerequisites, int[] count){
+        for(int idx = 0 ; idx<numCourses; idx++){
+            graph.add(new ArrayList<>());
+        }
+        for(int[] prerequisite : prerequisites){
+            int first = prerequisite[1];
+            int second = prerequisite[0];
+            count[second]++;
+            graph.get(first).add(second);
+        }
+
     }
 }
