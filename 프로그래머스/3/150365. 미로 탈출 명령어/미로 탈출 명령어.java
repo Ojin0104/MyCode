2d524c422c44445cb3714da1d4913f38f0c556c0
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2025-10-05 16:18:53 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 1.32 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,60 +1,46 @@
-import java.util.*;
-
 class Solution {
-   
-    
+
+    int[] dx = {1, 0, 0, -1};          // d, l, r, u
+    int[] dy = {0, -1, 1, 0};
+    char[] dir = {'d', 'l', 'r', 'u'};
+
+    int n, m, r, c, k;
+
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        String answer = "";
-        
-        //d l r u
-        
-        //S ~ E 합 구함
-        int moveR = r-x;
-        int moveC = c-y;
-        int nowR = x;
-        int nowC = y;
-        int shortestMove = Math.abs(moveR)+Math.abs(moveC);
+        this.n = n;
+        this.m = m;
+        this.r = r;
+        this.c = c;
+        this.k = k;
+
         StringBuilder sb = new StringBuilder();
-        if((k-shortestMove)%2==1||k<shortestMove)return "impossible";
-        for(int idx= 1; idx<=k; idx++){
-            
-            //System.out.println(moveR+" "+moveC+" "+(k-idx+1));
-            if(k-idx+1 == Math.abs(moveR)+Math.abs(moveC)){
-                if(moveR>0){
-                    sb.append("d");
-                    moveR--;
-                }else if(moveC<0){
-                    sb.append("l");
-                    moveC++;
-                }else if(moveC>0){
-                    sb.append("r");
-                    moveC--;
-                }else{
-                    sb.append("u");
-                    moveR++;
-                }
-                continue;
-            }
-            
-            if(nowR+1<=n){
-                sb.append("d");
-                nowR++;
-                moveR--;
-            }else if(nowC-1>=1){
-                sb.append("l");
-                nowC--;
-                moveC++;
-            }else if(nowC+1<=m){
-                sb.append("r");
-                nowC++;
-                moveC--;
-            }else{
-                sb.append("u");
-                nowR--;
-                moveR++;
-            }
-           // System.out.println(nowR+" "+nowC);
+
+        return dfs(x, y, sb) ? sb.toString() : "impossible";
+    }
+
+    // DFS로 사전순 경로를 탐색하며 유효한 경로를 찾으면 true 반환
+    public boolean dfs(int x, int y, StringBuilder sb) {
+        if (sb.length() == k) {
+            return x == r && y == c;
         }
-        return sb.toString();
+
+        int remain = k - sb.length();
+        int distance = Math.abs(x - r) + Math.abs(y - c);
+
+        // 가지치기: 남은 거리보다 많이 이동해야 하거나, 도달 가능성이 없는 경우
+        if (remain < distance || (remain - distance) % 2 != 0) return false;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 1 || nx > n || ny < 1 || ny > m) continue;
+
+            sb.append(dir[i]);
+            if (dfs(nx, ny, sb)) return true;
+            sb.deleteCharAt(sb.length() - 1);  // 백트래킹
+        }
+
+        return false;
     }
 }
