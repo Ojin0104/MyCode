595daf75098f798b18c93893671f9dfb6541795d
Yeongjin Han <81220859+Ojin0104@users.noreply.github.com>
2026-03-18 21:44:58 +0900

    Time: 0 ms (100%), Space: 43.3 MB (86.39%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,36 +1,27 @@
 class Solution {
-    private static final List<String>[] CHAR_LIST = new List[10];
-    
-    static {
-        CHAR_LIST[0] = Arrays.asList();
-        CHAR_LIST[1] = Arrays.asList();
-        CHAR_LIST[2] = Arrays.asList("a", "b", "c");
-        CHAR_LIST[3] = Arrays.asList("d", "e", "f");
-        CHAR_LIST[4] = Arrays.asList("g", "h", "i");
-        CHAR_LIST[5] = Arrays.asList("j", "k", "l");
-        CHAR_LIST[6] = Arrays.asList("m", "n", "o");
-        CHAR_LIST[7] = Arrays.asList("p", "q", "r","s");
-        CHAR_LIST[8] = Arrays.asList("t", "u", "v");
-        CHAR_LIST[9] = Arrays.asList("w", "x", "y", "z");
-    }
-    
     public List<String> letterCombinations(String digits) {
-        
+        char[][] numbers = {{},{},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'}
+        ,{'t','u','v'},{'w','x','y','z'}};
+
         List<String> answer = new ArrayList<>();
-        if(digits.isBlank())return answer;
-        backtracking(digits,0, answer, new StringBuilder());
+
+        backtracking(answer,numbers,new StringBuilder(),digits,0);
+
         return answer;
     }
 
-    public void backtracking(String digits,int idx, List<String> answer, StringBuilder str){
-        if(idx == digits.length()){
+    public void backtracking(List<String> answer, char[][] numbers, StringBuilder str, String digits, int idx){
+        if(idx>= digits.length()){
             answer.add(str.toString());
             return;
         }
-        int num = digits.charAt(idx) - '0';
-        for(String character: CHAR_LIST[num]){
-            backtracking(digits,idx+1,answer, str.append(character));
+        
+        int nowIdx = digits.charAt(idx)- '0';
+
+        for(char number : numbers[nowIdx]){
+            backtracking(answer,numbers,str.append(number),digits,idx+1);
             str.deleteCharAt(str.length()-1);
         }
+
     }
 }
